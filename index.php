f66c8873f5b2c40b72e59f7266f8552ac661ce5d
ittut <65811078+ittut@users.noreply.github.com>
2020-08-19 13:18:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<?php if( preg_match("/^\/index.php/",$_SERVER["REQUEST_URI"]) ) { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="refresh" content="0;http://it-tutorial.net<?php echo str_replace('/index.php','',$_SERVER["REQUEST_URI"]) ?>"/>
-</head>
-</html>
-
-<?php } else { ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
@@ -417,4 +409,3 @@ new TWTR.Widget({
     
 </body>
 </html>
-<?php } ?>
